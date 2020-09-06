0a7eff9969125028ed6f1513eb3af5d758caf91e
GitHub Action <action@github.com>
2020-09-06 12:02:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lemonnekogh/uefidevelopmentpluginforclion/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lemonnekogh.uefidevelopmentpluginforclion.services
+
+import com.github.lemonnekogh.uefidevelopmentpluginforclion.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/lemonnekogh/uefidevelopmentpluginforclion/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lemonnekogh/uefidevelopmentpluginforclion/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lemonnekogh.uefidevelopmentpluginforclion.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lemonnekogh.uefidevelopmentpluginforclion.MyBundle
 
 class MyProjectService(project: Project) {
 
